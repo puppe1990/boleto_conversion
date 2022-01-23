755cd086fc1ad3caa5838b2f4442fb6cc1ea5e53
puppe1990 <matheus.puppe90@hotmail.com>
2022-01-22 22:09:40 -0300

    add initial files to gem

---
+++ lib/boleto_conversion.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "boleto_conversion/version"
+
+module BoletoConversion
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/boleto_conversion_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe BoletoConversion do
+  it "has a version number" do
+    expect(BoletoConversion::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
